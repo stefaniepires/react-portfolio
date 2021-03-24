7a9f667c5726da7f0e7d50077086e785ecc57704
steftinsley <69828366+steftinsley@users.noreply.github.com>
2021-03-24 16:22:37 -0500

    added css styling to the sidebar

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,20 +1,39 @@
 import React from 'react'
+import './Profile.css';
 import headshot from '../../assets/headshot.png';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
+import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
+
+
+
+
 
 const Profile = () => {
   return (
-    <div>
+    <div className='profile container_shadow'>
       <center>
+        <div className='headshot'>
       <img src={headshot} alt="Headshot" width="200"/>
-      <h2>Stefanie Pires</h2>
-      <p><em>Full Stack Developer</em></p>
+      </div>
+      <div className='profileHeader'>
+       <div className='name'> 
+      Stefanie Pires
+      </div>
+      <div className='jobTitle'>
+      
+        Full Stack Developer
       <br/>
+    </div>
+      </div>
+      <div>
       <p>
       Motivated and creative Full Stack Developer passionate about creating user-friendly software.
       </p>
       <br/>
       <p><LocationOnIcon/>Dallas-Ft. Worth, TX</p>
+      <br/>
+      <button>Download CV<CloudDownloadIcon/></button> 
+      </div>
       </center>
     </div>
   )
